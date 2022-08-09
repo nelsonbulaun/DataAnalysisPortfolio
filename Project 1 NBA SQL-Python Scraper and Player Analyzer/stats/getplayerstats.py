0a57f77310d17639f796876de7f544998958865b
nelsonbulaun <108250880+nelsonbulaun@users.noreply.github.com>
2022-08-09 16:58:54 -0700

    Add files via upload

--- Project 1 NBA SQL-Python Scraper and Player Analyzer/stats/getplayerstats.py
+++ Project 1 NBA SQL-Python Scraper and Player Analyzer/stats/getplayerstats.py
@@ -15,49 +15,60 @@ database = 'nba_stats'
 
 playerdata = []
 namedata = []
-def getplayerstats(fname):
+
+testlist = ["Joel Embiid" , "Stephen Curry", "Seth Curry"]
+
+
+
+def makeplayers(listofplayers):
+  playerssearched = []
+  for pdata in listofplayers:
+    playerselected = PlPosition(pdata[0], pdata[1], pdata[2], pdata[3], pdata[4], pdata[5], pdata[6], pdata[7], pdata[8], pdata[9], pdata[10])
+    playerssearched.append(playerselected)
+  return(playerssearched)
+
+def getplayersstats(listofnames):
   con = pymysql.connect(
   host=hostname,
   user=username,
   password=password,
-  database=database
-)
+  database=database)
   try:
     with con.cursor() as cur:
-        testa=[]
-        for catagory in catagory_list:
-          test1 = ("SELECT avg("+catagory+") FROM nba_stats.player_general_traditional_totals_positions WHERE player_name= '"+fname+"' AND season_id='2021-22'")
-          test2 = ("SELECT player_name FROM nba_stats.player_general_traditional_totals_positions WHERE player_name= '"+fname+"' AND season_id='2021-22'")
-          test3 = ("SELECT player_position FROM nba_stats.player_general_traditional_totals_positions WHERE player_name= '"+fname+"' AND season_id='2021-22'")
-          if catagory == 'fg3m':
-            cur.execute(test2)
-            rowa = cur.fetchall()
-            cur.execute(test3)
-            rowb = cur.fetchall()            
-            cur.execute(test1)
-            rowc = cur.fetchall()
-            # print(rowa)
-            # print(rowb)
-            testa.extend([rowa[0], rowb[0],rowc[0]])
-            # print(testa)
-          else:
-            cur.execute(test1)
-            rows = cur.fetchall()
-            testa.append(rows[0])
-            # print(testa)
-        y = [i[0] for i in testa]
-        playerdata.append(y)
-        #print(playerdata)
-        pdata=playerdata[0]
-        playerselected = PlPosition(pdata[0], pdata[1], pdata[2], pdata[3], pdata[4], pdata[5], pdata[6], pdata[7], pdata[8], pdata[9], pdata[10])
-        #print(playerselected)
-        return(playerselected)
-
- 
+      for fname in listofnames:
+          testa=[]
+          for catagory in catagory_list:
+            test1 = ("SELECT avg("+catagory+") FROM nba_stats.player_general_traditional_totals_positions WHERE player_name= '"+fname+"' AND season_id='2021-22'")
+            test2 = ("SELECT player_name FROM nba_stats.player_general_traditional_totals_positions WHERE player_name= '"+fname+"' AND season_id='2021-22'")
+            test3 = ("SELECT player_position FROM nba_stats.player_general_traditional_totals_positions WHERE player_name= '"+fname+"' AND season_id='2021-22'")
+            if catagory == 'fg3m':
+              cur.execute(test2)
+              rowa = cur.fetchall()
+              cur.execute(test3)
+              rowb = cur.fetchall()            
+              cur.execute(test1)
+              rowc = cur.fetchall()
+              testa.extend([rowa[0], rowb[0], rowc[0]])
+            else:
+              cur.execute(test1)
+              rows = cur.fetchall()
+              testa.append(rows[0])
+              # print(testa)
+          y = [i[0] for i in testa]
+          playerdata.append(y)
+          print(playerdata)
+    return makeplayers(playerdata)
+                    
   finally:
-
     con.close()
+  
 
 
-# a=(getplayerstats('Joel Embiid'))
-# print(a)
+
+#a = getplayersstats(testlist)
+#print(a)
+
+#a=(getplayerstats('Joel Embiid'))
+#print(a)
+#print(a.position)
+
--- Project 1 NBA SQL-Python Scraper and Player Analyzer/stats/playercomparetable.py
+++ Project 1 NBA SQL-Python Scraper and Player Analyzer/stats/playercomparetable.py
@@ -2,23 +2,89 @@ from __future__ import print_function
 from audioop import avg
 import pymysql
 import pandas as pd
+import jinja2
+import matplotlib
+import matplotlib.cm
 
 from getaveragestats import PlPosition
 from getaveragestats import ListOfStatPositions
-from getplayerstats import getplayerstats
+from getplayerstats import getplayersstats
+
+CatList = ["Three Pointers Made",  
+    "Fieldgoal Percentage", 
+    "Freethrow Percentage", 
+    "Rebounds",
+    "Assists",
+    "Turnovers",
+    "Steals", 
+    "Blocks",
+    "Points"
+]
 
 
-def playercomparetable(zname):
-    z = getplayerstats(zname)
-    ComparingList = [
-    z, 
-    ListOfStatPositions[0],
-    ListOfStatPositions[1],
-    ListOfStatPositions[2],
-    ListOfStatPositions[3]]
+def background_with_norm(s):
+    cmap = matplotlib.cm.get_cmap('RdYlGn')
+    norm = matplotlib.colors.TwoSlopeNorm(vmin=s[0]*0.75, vcenter=s[0], vmax=s[0]*1.25)
+    #norm = matplotlib.colors.TwoSlopeNorm(vmin=s[1]*0.75, vcenter=s[1], vmax=s.values.max())
+    return ['background-color: {:s}'.format(matplotlib.colors.to_hex(c.flatten())) for c in cmap(norm(s.values))]
+
+styles = [
+    dict(selector="tr:hover",
+                props=[("background", "#f4f4f4")]),
+    dict(selector="th", props=[("color", "#fff"),
+                               ("border", "1px solid #eee"),
+                               ("padding", "10px 20px"),
+                               ("border-collapse", "collapse"),
+                               ("background", "#17408B"),
+                               ("text-transform", "uppercase"),
+                               ("font-size", "13px")
+                               ]),
+    dict(selector="td", props=[("color", "#000000"),
+                               ("border", "1px solid #eee"),
+                               ("padding", "12px 35px"),
+                               ("border-collapse", "collapse"),
+                               ("font-size", "15px")
+                               ]),
+    dict(selector="table", props=[
+                                    ("font-family" , 'Arial'),
+                                    ("margin" , "25px auto"),
+                                    ("border-collapse" , "collapse"),
+                                    ("border" , "1px solid #eee"),
+                                    ("border-bottom" , "2px solid #00cccc"),                                    
+                                      ]),
+    dict(selector="caption", props=[("caption-side", "bottom")])
+]
+
+ComparingList = [ ]
+
+bestlist = ["Stephen Curry", "Zach LaVine", "Donovan Mitchell"]
+
+def comparemultiple(listofnames):
+    listofstats = getplayersstats(listofnames)
+    if listofstats[0].position == "G":
+        ComparingList.append(ListOfStatPositions[1])
+        ComparingList.extend(listofstats)
+        ComparingList.append(ListOfStatPositions[0])
+        print(ComparingList) 
+    elif listofstats[0].position == "F":
+        ComparingList.append(ListOfStatPositions[2])
+        ComparingList.extend(listofstats)
+        ComparingList.append(ListOfStatPositions[0])
+        print(ComparingList)
+    elif listofstats[0].position == "C":
+        ComparingList.append(ListOfStatPositions[3])
+        ComparingList.extend(listofstats)
+        ComparingList.append(ListOfStatPositions[0])
+        print(ComparingList)
     df = pd.DataFrame.from_records([x.as_dict() for x in ComparingList])
+    html = df.style.set_table_styles(styles).apply(background_with_norm, subset = pd.IndexSlice[0:len(listofnames),["Three Pointers Made",  "Fieldgoal Percentage", "Freethrow Percentage", "Rebounds","Assists","Turnovers","Steals", "Blocks","Points"]]).to_html()
+    text_file = open("playercomparetable.html", "w")
+    text_file.write(html)
+    text_file.close()
     print(df)
 
-#playercomparetable("Stephen Curry")
- 
+
+comparemultiple(bestlist)
+
+
 
